5eb71bed780bab1ff0a8f044613808173945115c
im-Ahmed <ahmedfarooqim3@example.com>
2025-06-26 16:23:26 +0500

    Fix case-sensitive path and folder name issues in NewCollection

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Hero/Hero";
 import Popular from "../components/Popular/Popular";
 import Offers from "../components/Offers/Offers";
-import NewCollection from "../components/New Collections/NewCollection";
+import NewCollection from "../components/NewCollections/NewCollection";
 import NewsLetter from "../components/NewsLetter/NewsLetter";
 
 const Shop = () => {
