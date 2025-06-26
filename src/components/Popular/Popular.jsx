89dd18d7775aabf57a403f2f218d7b930613365c
im-Ahmed <ahmedfarooqim3@example.com>
2025-06-26 16:26:47 +0500

    Fix case-sensitive path and folder name issues in NewCollection

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Popular.css";
 import data_product from "../assets/data";
-import Item from "../Items/item";
+import Item from "../Items/Item";
 
 const Popular = () => {
   return (
